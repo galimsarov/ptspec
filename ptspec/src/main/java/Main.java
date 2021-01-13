03dc3af817581ee0ded068bc4a2b56b9abd96fa9
egalimsarov <e.galimsarov@gmail.com>
2021-01-13 15:28:31 +0500

    Реализован метод чтения файлов из архива

--- ptspec/src/main/java/Main.java
+++ ptspec/src/main/java/Main.java
@@ -1,5 +1,40 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
 public class Main {
+    private final static String archName = "source_archive.zip";
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        readArchive();
+    }
+
+    /**
+     * Метод readArchive()
+     * Читаем файлы из архива
+     */
+    public static void readArchive() {
+        try(ZipInputStream zin = new ZipInputStream(new FileInputStream(archName))) {
+            ZipEntry entry;
+            String name;
+            long size;
+            while ((entry = zin.getNextEntry()) != null) {
+                name = entry.getName(); // получим название файла
+                size = entry.getSize();  // получим его размер в байтах
+                System.out.printf("File name: %s \t File size: %d \n", name, size);
+                // распаковка
+                FileOutputStream fout = new FileOutputStream("new" + name);
+                for (int c = zin.read(); c != -1; c = zin.read()) {
+                    fout.write(c);
+                }
+                fout.flush();
+                zin.closeEntry();
+                fout.close();
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
