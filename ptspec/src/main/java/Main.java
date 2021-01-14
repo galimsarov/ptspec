c018329fee20ba6647211e3a81d801cfe9510368
egalimsarov <e.galimsarov@gmail.com>
2021-01-14 16:19:07 +0500

    Вывод отчётов в файлы

--- ptspec/src/main/java/Main.java
+++ ptspec/src/main/java/Main.java
@@ -1,5 +1,9 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
@@ -12,6 +16,7 @@ public class Main {
     public static void main(String[] args) {
         List<String> fileNames = readArchive();
         Map<String, List<Integer>> outputDate = readFiles(fileNames);
+        processingResult(outputDate);
     }
 
     /**
@@ -49,9 +54,8 @@ public class Main {
     }
 
     /**
-     * Метод readFiles()
-     * Читаем данные из файлов, ищем совпадения в
-     * @see Label
+     * Метод readFiles(List<String> fileNames)
+     * Читаем данные из файлов, сводим в один мап
      */
     public static Map<String, List<Integer>> readFiles(List<String> fileNames) {
         Map<String, List<Integer>> outputDate = new HashMap<>();
@@ -83,5 +87,57 @@ public class Main {
         for (String key: outputDate.keySet())
             System.out.println(key + " " + outputDate.get(key));
         return outputDate;
+    }
+
+    /**
+     * Метод processingResult(Map<String, List<Integer>> outputDate)
+     * Выводим три варианта итогового отчёта
+     */
+    public static void processingResult(Map<String, List<Integer>> outputDate) {
+        try {
+            System.out.println("------");
+            FileWriter writer = new FileWriter("src/main/resources/final_report_var1.json");
+            GsonBuilder builder = new GsonBuilder();
+            Gson gson = builder.serializeNulls().setPrettyPrinting().create();
+            Map<String, Integer> temp = new TreeMap<>();
+            for (String label : outputDate.keySet())
+                temp.put(label, outputDate.get(label).stream().mapToInt(i -> i).sum());
+            String json = gson.toJson(temp);
+            writer.write(json);
+            writer.flush();
+            System.out.println("В json файл записан отчёт по варианту 1");
+
+            writer = new FileWriter("src/main/resources/final_report_var2.json");
+            temp = new TreeMap<>();
+            for (Label value : Label.values())
+                if (outputDate.containsKey(value.toString()))
+                    temp.put(value.toString(),
+                            outputDate.get(value.toString()).stream().mapToInt(i -> i).sum());
+                else
+                    temp.put(value.toString(), null);
+            json = gson.toJson(temp);
+            writer.write(json);
+            writer.flush();
+            System.out.println("В json файл записан отчёт по варианту 2");
+
+            writer = new FileWriter("src/main/resources/final_report_var3.json");
+
+            Map<String, Integer[]> newTemp = new TreeMap<>();
+            for (String label : outputDate.keySet()) {
+                int[] primArray = outputDate.get(label).stream().mapToInt(i -> i).toArray();
+                Integer[] objArray = Arrays.stream(primArray).boxed().toArray(Integer[]::new);
+                Arrays.sort(objArray, Collections.reverseOrder());
+                newTemp.put(label, objArray);
+            }
+            json = gson.toJson(newTemp);
+            writer.write(json);
+            writer.flush();
+            System.out.println("В json файл записан отчёт по варианту 3");
+
+            writer.close();
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
